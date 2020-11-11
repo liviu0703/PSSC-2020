5c5ed5cf4345e3ab0a1ffbf7d858a958b51d494b
liviu0703 <55464964+liviu0703@users.noreply.github.com>
2020-11-11 17:30:20 +0200

    Tema 6

---
+++ Dumitrasc-Liviu/L06/ReplyWorkflow/Outputs/CreateReplyResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReplyWorkflow.Models;
+
+namespace ReplyWorkflow.Outputs
+{
+
+    public static partial class CreateReplyResult
+    {
+        public interface ICreateReplyResult { }
+
+        public class ReplyValid : ICreateReplyResult
+        {
+            public Reply Reply { get; }
+
+            public ReplyValid(Reply reply)
+            {
+                Reply = reply;
+            }
+        }
+
+        public class ReplyInvalid : ICreateReplyResult
+        {
+            public string Reasons { get; }
+
+            public ReplyInvalid(string reasons)
+            {
+                Reasons = reasons;
+            }
+        }
+
+        public class InvalidRequest : ICreateReplyResult
+        {
+            public string Msg { get; }
+
+            public InvalidRequest(string msg)
+            {
+                Msg = msg;
+            }
+        }
+
+    }
+}
